e92804973087358de3f4f3bb69a214ee52e429ef
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2022-01-12 11:26:40 +0300

    initial commit

---
+++ Exploratory/plot2.R
@@ -0,0 +1,19 @@
+library("data.table")
+
+df <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+
+df[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+df[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+df <- df[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", height=480, width=480)
+
+plot(x = df[, dateTime]
+     , y = df[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
